b3e11de06c3814c0a16ccfc34953af2b11b13ca0
yuzhenwei <yzw0504@163.com>
2017-10-09 16:55:26 +0800

    TestNg与maven初步整合

---
+++ ngPritise/src/test/resources/com/qlk/inter/InterfaceTestNG.java
@@ -0,0 +1,44 @@
+package com.qlk.inter;
+
+
+
+import java.util.Properties;
+
+import org.testng.annotations.Test;
+
+public class InterfaceTestNG {
+	
+
+	   private Properties properties;
+
+	@Test  
+	    public String getName() {  
+	        System.err.println("return name.... getName()");  
+	        return "name";  
+	    }  
+	  
+	    @Test  
+	    public void funtest() {  
+	        System.err.println("this is funtest......");  
+	    }  
+	  
+	    @Test  
+	    public void saveMethod1() {  
+	        System.err.println("this is saveMethod1......"); 
+	        properties = System.getProperties();
+	        System.out.println(properties.toString());
+	    }  
+	  
+	    @Test  
+	    public void saveMethod2() {  
+	        System.err.println("this is saveMethod2......");  
+	    }  
+	  
+	    @Test  
+	    public void saveMethod3() {  
+	        System.err.println("this is saveMethod3......");  
+	    }  
+	  
+	}  
+
+
